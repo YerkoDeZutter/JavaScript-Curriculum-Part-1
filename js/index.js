86dc2039705a4b7088ba6dba32f3fcea9162ccf2
Yerko <yerkodezutter@gmail.com>
2019-01-14 09:55:15 +0100

    tekoop dividet in 2 + if statements

--- js/index.js
+++ js/index.js
@@ -4,10 +4,40 @@ function CChar(name, age, items_to_give){
   this.age = age;
   this.items_to_give = items_to_give;
 
-  console.log("hello my name is " + name)
-  console.log("my age is " + age)
-  console.log("and the i get is " + name)
+  console.log("hello my name is " + this.name);
+  console.log("my age is " + this.age);
+  console.log("and the i get is " + this.items_to_give);
 
 }
 
-let character = new CChar("zip", 18, "ziper")
+function shop(naam, physic, magic, minLevel, available){
+
+  this.naam = naam;
+  this.physic = physic;
+  this.magic = magic;
+  this.minLevel = minLevel;
+  this.available = available;
+
+  // if(this.available){
+  //   console.log(this.naam + " " + this.physic + " " + this.magic + " " + this.minLevel + " " + this.available);
+  // }
+
+  if(this.minLevel > 9){
+    console.log(this.naam + " " + this.physic + " " + this.magic + " " + this.minLevel + " " + this.available);
+  }
+
+}
+
+let tekoop = [];
+
+for( var i = 0 ; i < 5 ; i++ ){
+  tekoop.push(new shop("shrunken heads", 11, 12, 1, true))
+}
+
+for( var i = 0 ; i < 5 ; i++ ){
+  tekoop.push(new shop("axe", 110, 120, 10, false))
+}
+
+//"shrunken heads", "brooms", "axe"
+
+let character = new CChar("zip", 18, ["ziper", "rits"]);
