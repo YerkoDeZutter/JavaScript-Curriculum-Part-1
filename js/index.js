b41eee2586c119f80b6fd804625ce53e2dc35808
Yerko <yerkodezutter@gmail.com>
2019-01-14 09:24:56 +0100

    working begin code

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function CChar(name, age, items_to_give){
+
+  this.name = name;
+  this.age = age;
+  this.items_to_give = items_to_give;
+
+  console.log("hello my name is " + name)
+  console.log("my age is " + age)
+  console.log("and the i get is " + name)
+
+}
+
+let character = new CChar("zip", 18, "ziper")
